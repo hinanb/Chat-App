dcd543a8d8094784ebfe3eb295990de35859ca59
hinanb <67995872+hinanb@users.noreply.github.com>
2020-08-17 12:39:19 +0500

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ import socket
 import threading
 import time
 #db config
-import mysql.connector
-mydb = mysql.connector.connect(
+import mysql.connector                
+mydb = mysql.connector.connect(   #db connection
   host="localhost",
   user="root",
   database="cnet"
@@ -11,10 +11,10 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 
-tLock = threading.Lock()
+tLock = threading.Lock()      #multithreading lock for avoiding race cond
 shutdown = False
 
-def receving(name, sock):
+def receving(name, sock):    #function for recieving messages
     while not shutdown:
         try:
             tLock.acquire()
@@ -24,15 +24,15 @@ def receving(name, sock):
         except:
             pass
         finally:
-            tLock.release()
+            tLock.release() #multithreading lock for avoiding race cond
 ''' for diff comm communication
 host = '127.168.10.7'
 port = 4005
 server = ('127.168.10.10',4000)
 ''' #ends here
 
-host = '127.0.0.1' 
-port = 5005 
+host = '127.0.0.1'
+port = 5005 # Port assigned after the range of reserved ports i.e. 1025
 
 server = ('127.0.0.1',5000)
 
@@ -44,7 +44,7 @@ rT = threading.Thread(target=receving, args=("RecvThread",s))
 rT.start()
 
 alias = input("Name: ")
-mycursor.execute("SELECT * FROM chat where user= '%s'" % alias)
+mycursor.execute("SELECT * FROM chat where user= '%s'" % alias) #querry for fetching all records of a user
 
 myresult = mycursor.fetchall()
 try:
@@ -54,7 +54,7 @@ except:
     print("New User")
 
 message = input(alias + "-> ")
-while message != 'q':
+while message != 'q':   #loop untill user quits
     if message != '':
         s.sendto(bytes(alias + ": " + message, 'utf-8'), server)
         sql = "INSERT INTO chat (user, message) VALUES (%s, %s)"
@@ -62,7 +62,7 @@ while message != 'q':
         mycursor.execute(sql, val)
         mydb.commit()
 
-    tLock.acquire()
+    tLock.acquire() #threading for avoiding race condition between differnt users texting at same time
     message = input(alias + "-> ")
     if message =='f':
         filesignal='f'
@@ -75,7 +75,6 @@ while message != 'q':
         
         #s.sendto(bytes(file_data),server)
         print("Data has been transmitted successfully")
-
     tLock.release()
     time.sleep(0.2)
 
